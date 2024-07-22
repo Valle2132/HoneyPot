95e2c2883976925d93e0c1464eea69d8a19d5035
Valentin Fritz <valentinfritz@gmx.at>
2024-07-22 22:44:48 +0200

    game movements

--- HoneyPotGame/scripts/EnemyScene.cs
+++ HoneyPotGame/scripts/EnemyScene.cs
@@ -4,12 +4,27 @@ using System;
 public partial class EnemyScene : RigidBody2D
 {
 	[Export]
-	public float GravityScale = 1.0f;
-
+	public float GravityScale = 0.5f;
+	
 	private Vector2 gravity;
 
 	public override void _Ready()
 	{
 		gravity = new Vector2(0, 980) * GravityScale;
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		if (MoveAndCollide(gravity * (float)delta) != null)
+		{
+			KillEnemy();
+		}
+	}
+	
+	private void KillEnemy()
+	{
+		QueueFree();
+		
+		// TODO: add sound
 	}
 }
--- HoneyPotGame/scripts/PlayerScene.cs
+++ HoneyPotGame/scripts/PlayerScene.cs
@@ -3,8 +3,10 @@ using System;
 
 public partial class PlayerScene : CharacterBody2D
 {
-	[Export]
-	public const float Speed = 400.0f;
+	public const float Speed = 300.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	//public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _PhysicsProcess(double delta)
 	{
